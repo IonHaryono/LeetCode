8490a7852635693956a9781dcbe9f9b48573dba6
IonHaryono <ion.haryono@gmail.com>
2023-05-15 23:20:52 +1000

    Create GithubLeetCodeCheck.py

---
+++ GithubLeetCodeCheck.py
@@ -0,0 +1,48 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+
+options = Options()
+options.headless = True
+options.add_argument("--window-size=1920,1200")
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')
+driver = webdriver.Chrome(options=options)
+
+user1 = "IonH"
+user2 = "eric-yem"
+user3 = "ZeloSama"
+userList = [user1, user2, user3]
+print(userList)
+
+for i in range(len(userList)):
+    URL = "https://leetcode.com/" + userList[i]
+    driver.get(URL)
+    # driver.implicitly_wait(5)
+
+    secondProblem = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[2]/div/div[2]/div[3]/div/div/div[2]/a[2]/div/span[2]')
+    secondTime = secondProblem.text.strip().split(" ")[1]
+    # print(secondTime)
+    if secondTime == "days" or secondTime == "months":
+        # print(userList[i] + " has NOT done 2 problems.")
+
+        firstProblem = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[2]/div/div[2]/div[3]/div/div/div[2]/a[1]/div/span[2]')
+        firstTime = firstProblem.text.strip().split(" ")[1]
+        if firstTime == "days" or firstTime == "months":
+            print(userList[i] + " has not done any problems today.")
+        else: 
+            print(userList[i] + " has done only 1 problem today.")
+
+    else:
+        print(userList[i] + " has done 2 problems today.")
+
+
+
+
+
+
+driver.quit()
